9aa213ef84975bd02b1ef72ae31810687951e4f5
mrx1998 <mrx1998@126.com>
2020-03-09 12:26:52 +0800

    hello

---
+++ test.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(){
+    int a;
+    printf("请输入您的岁数:");
+    scanf("%d",&a);
+    printf("您的岁数是:%d\n",a);
+    system("pause");
+    return 0;
+}
